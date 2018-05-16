560d82ffeca7a51572e06123806f9271339b32e3
José Navarro <josenava@estudiantec.cr>
2018-05-16 13:20:21 -0600

    ...

--- Codigo/Proyecto2.pl
+++ Codigo/Proyecto2.pl
@@ -22,7 +22,7 @@ resolver(Acertijo, Structure, Solucion) :-
          mostrar(Structure).              % Muestra estructura en forma legible.
 
 
-structure(smoothies, [orden(5, _, _, _),
+structure(smoothies, [%orden(5, _, _, _),
                       orden(6, _, _, _),
                       orden(7, _, _, _),
                       orden(8, _, _, _),
@@ -39,11 +39,11 @@ clues(
      pista3(Smoothies),
      pista4(Smoothies),
      pista5(Smoothies),
-     % pista6(Smoothies),
-     pista7(Smoothies)
-     % pista8(Smoothies),
+     pista6(Smoothies),
+     pista7(Smoothies),
+     pista8(Smoothies),
      % pista9(Smoothies),
-     % pista10(Smoothies)
+     pista10(Smoothies)
      % Pregunta: 11.    ¿Quién pidió mandarina?
    ]).
    
@@ -79,10 +79,10 @@ pista1(E):-precio(6,V1), select(V1,E,E2),
 
 % 2. El cliente que ordenó semilla de lino pagó más que la persona que ordenó pasto de trigo.
 pista2(E):-superAlimento(semillaDeLino,V1), precio(PrecioSemillaDeLino,V1),
+           member(V1,E), select(V1,E,E2),
            superAlimento(pastoDeTrigo,V2), precio(PrecioPastoDeTrigo,V2),
-           PrecioSemillaDeLino > PrecioPastoDeTrigo,
-           member(V1,E), member(V2,E).
-
+           member(V2,E2), select(V2,E2,_),
+           PrecioSemillaDeLino > PrecioPastoDeTrigo.
 
 % 3. Isabel pidió semillas de chia.
 pista3(E):-cliente(isabel,V1),superAlimento(semillasDeChia,V1),member(V1,E).
@@ -102,8 +102,10 @@ pista5(E):-fruta(arandanos,V1), select(V1,E,E2),
 
 % 6. El cliente que pidió naranjas pagó 1 dólar más que la persona que pidió bananos.
 pista6(E):-fruta(naranjas,V1), precio(PrecioNaranjas,V1),
+           member(V1,E), select(V1,E,E2),
            fruta(bananos,V2), precio(PrecioBananos,V2),
-           (PrecioNaranjas + 1) = PrecioBananos, member(V1,E).
+           member(V2,E2), select(V2,E2,_),
+           (PrecioNaranjas + 1) = PrecioBananos.
 
 % 7. Otis, o pagó $6 o pagó $10.
  % - Otis pagó $6
@@ -114,15 +116,19 @@ pista7(E):-cliente(otis,V1),precio(10,V1),member(V1,E).
 
 % 8. La persona que pidió quinoa pagó $3 más que Mercedes.
 pista8(E):-superAlimento(quinoa,V1), precio(PrecioQuinoa,V1),
+           member(V1,E), select(V1,E,E2),
            cliente(mercedes,V2), precio(W,V2),
-           (W + 3) = PrecioQuinoa, member(V1,E).
+           member(V2,E2), select(V2,E2,_),
+           (W + 3) = PrecioQuinoa.
 
 % 9. Sobre Paulette y la persona que ordenó frambuesas: una pidió pasto de trigo y la otra persona pagó $8.
 
 % 10. Isabel pagó 3 dólares menos que Amelia.
 pista10(E):-cliente(amelia,V1), precio(Q,V1),
+            member(V1,E), select(V1,E,E2),
             cliente(isabel,V2), precio(P,V2),
-            (P - 3) = Q, member(V2,E).
+            member(V2,E2), select(V2,E2,_),
+            (P - 3) = Q.
 
 
 
@@ -130,3 +136,5 @@ cliente(Cliente,orden(_,Cliente,_,_)).
 superAlimento(SuperAlimento,orden(_,_,SuperAlimento,_)).
 fruta(Fruta,orden(_,_,_,Fruta)).
 precio(Precio,orden(Precio,_,_,_)).
+
+% structure(compras,E), pista(E).
